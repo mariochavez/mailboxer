674fe1ca4a4bcd9cd271f9fb7c0856d0737b79a4
Brad Haydon <bradhaydon@gmail.com>
2015-05-13 00:40:59 -0700

    Update 20131206080416_add_conversation_optout.rb

--- db/migrate/20131206080416_add_conversation_optout.rb
+++ db/migrate/20131206080416_add_conversation_optout.rb
@@ -4,11 +4,11 @@ class AddConversationOptout < ActiveRecord::Migration
       t.references :unsubscriber, :polymorphic => true
       t.references :conversation
     end
-    add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", :name => "mb_opt_outs_on_conversations_id", :column => "conversation_id"
+    # add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", :name => "mb_opt_outs_on_conversations_id", :column => "conversation_id"
   end
 
   def self.down
-    remove_foreign_key "mailboxer_conversation_opt_outs", :name => "mb_opt_outs_on_conversations_id"
+    # remove_foreign_key "mailboxer_conversation_opt_outs", :name => "mb_opt_outs_on_conversations_id"
     drop_table :mailboxer_conversation_opt_outs
   end
 end
